1cddded47e9e26edc9b86f450e3bf2eff8d175be
chander <jpochander@gmail.com>
2025-12-05 14:08:30 +0200

    ref base27 digit2

--- digits/base27/dd2.go
+++ digits/base27/dd2.go
@@ -8,50 +8,148 @@ import (
 
 type DigitDrawer2 struct{}
 
-func (DigitDrawer2) DrawDigit(c *gg.Context, b geom.Bounds, digit int) {
+func (dd DigitDrawer2) DrawDigit(c *gg.Context, b geom.Bounds, digit int) {
+	//dd.drawDigit_v1(c, b, digit)
+	dd.drawDigit_v2(c, b, digit)
+}
+
+// func (DigitDrawer2) drawDigit_v1(c *gg.Context, b geom.Bounds, digit int) {
+
+// 	b = geom.BoundsAspect(b, AspectRatio)
+
+// 	const (
+// 		koefDA = 0.4
+// 		//koefDA = 0.5
+
+// 		koefLineWidth = 0.75
+// 		//koefLineWidth = 0.8
+// 		//koefLineWidth = 0.9
+// 		//koefLineWidth = 1.0
+
+// 		//enableAll = true
+// 		enableAll = false
+// 	)
+
+// 	var (
+// 		w = min(b.Dx(), b.Dy()/2)
+
+// 		dA = w / 2 * koefDA
+// 		dB = (w/2 - dA) / 2
+
+// 		xA = dA
+// 		xB = dB
+
+// 		yA = dA
+// 		yB = dB
+
+// 		lineWidth = dB * koefLineWidth
+// 	)
+
+// 	c.SetLineCap(gg.LineCapRound)
+// 	c.SetLineWidth(lineWidth)
+
+// 	center := b.Center()
+
+// 	// Draw vertical lines
+// 	if true {
+// 		c.MoveTo(center.X, center.Y-(2*yA+yB+yB/2))
+// 		c.LineTo(center.X, center.Y-(yB/2))
+
+// 		c.MoveTo(center.X, center.Y+(yB/2))
+// 		c.LineTo(center.X, center.Y+(2*yA+yB+yB/2))
+// 	}
+
+// 	trits := calcTritsBase27(digit)
+
+// 	var (
+// 		x1 = xB
+// 		x2 = xA + xB
+
+// 		y0 = center.Y - (2*yA + yB + yB/2)
+// 		y  = y0
+// 		dy = yA + yB
+// 	)
+// 	for i, trit := range trits {
+// 		if (i % 2) == 0 {
+// 			if enableAll || (trit == -1) {
+// 				c.MoveTo(center.X-x1, y+0*yA)
+// 				c.LineTo(center.X-x2, y+1*yA)
+// 			}
+// 			if enableAll || (trit == +1) {
+// 				c.MoveTo(center.X+x1, y+0*yA)
+// 				c.LineTo(center.X+x2, y+1*yA)
+// 			}
+// 		} else {
+// 			if enableAll || (trit == -1) {
+// 				c.MoveTo(center.X-x2, y+0*yA)
+// 				c.LineTo(center.X-x1, y+1*yA)
+// 			}
+// 			if enableAll || (trit == +1) {
+// 				c.MoveTo(center.X+x2, y+0*yA)
+// 				c.LineTo(center.X+x1, y+1*yA)
+// 			}
+// 		}
+// 		y += dy
+// 	}
+
+// 	c.Stroke()
+// }
+
+func (DigitDrawer2) drawDigit_v2(c *gg.Context, b geom.Bounds, digit int) {
 
 	b = geom.BoundsAspect(b, AspectRatio)
 
-	const (
-		koefDA = 0.4
-		//koefDA = 0.5
+	v := b.Vmin()
 
-		koefLineWidth = 0.75
-		//koefLineWidth = 0.8
-		//koefLineWidth = 0.9
-		//koefLineWidth = 1.0
+	c.Push()
+	defer c.Pop()
 
-		//enableAll = true
-		enableAll = false
+	c.Translate(b.Min.X, b.Min.Y)
+	c.Scale(v, v)
+
+	// size 100x200 : dx = 100, dy = 200
+
+	var (
+		// dA              = 20.0
+		// dB              = 20.0
+		// lineWidthFactor = 1.0
+
+		// dA            = 20.0
+		// dB            = 15.0
+		// lineWidthFactor = 0.8
+
+		dA              = 20.0
+		dB              = (50 - dA) / 2
+		lineWidthFactor = 0.8
 	)
 
 	var (
-		w = min(b.Dx(), b.Dy()/2)
-
-		dA = w / 2 * koefDA
-		dB = (w/2 - dA) / 2
-
 		xA = dA
 		xB = dB
 
 		yA = dA
 		yB = dB
 
-		lineWidth = dB * koefLineWidth
+		lineWidth = dB * lineWidthFactor
+
+		//enableAll = true
+		enableAll = false
 	)
 
 	c.SetLineCap(gg.LineCapRound)
-	c.SetLineWidth(lineWidth)
+	c.SetLineWidth(lineWidth * v)
 
-	center := b.Center()
+	center := geom.MakePoint2f(50, 100)
+
+	yH := 2*yA + yB + yB/2
 
 	// Draw vertical lines
 	if true {
-		c.MoveTo(center.X, center.Y-(2*yA+yB+yB/2))
+		c.MoveTo(center.X, center.Y-yH)
 		c.LineTo(center.X, center.Y-(yB/2))
 
 		c.MoveTo(center.X, center.Y+(yB/2))
-		c.LineTo(center.X, center.Y+(2*yA+yB+yB/2))
+		c.LineTo(center.X, center.Y+yH)
 	}
 
 	trits := calcTritsBase27(digit)
@@ -60,7 +158,7 @@ func (DigitDrawer2) DrawDigit(c *gg.Context, b geom.Bounds, digit int) {
 		x1 = xB
 		x2 = xA + xB
 
-		y0 = center.Y - (2*yA + yB + yB/2)
+		y0 = center.Y - yH
 		y  = y0
 		dy = yA + yB
 	)
@@ -89,74 +187,3 @@ func (DigitDrawer2) DrawDigit(c *gg.Context, b geom.Bounds, digit int) {
 
 	c.Stroke()
 }
-
-// func (DigitDrawer2) DrawDigit(c *gg.Context, b geom.Bounds, digit int) {
-
-// 	b = geom.BoundsAspect(b, AspectRatio)
-// 	v := b.Vmin()
-
-// 	c.Push()
-// 	defer c.Pop()
-
-// 	c.Translate(b.Min.X, b.Min.Y)
-// 	c.Scale(v, v)
-
-// 	lw := 10.0
-// 	c.SetLineWidth(lw * v)
-// 	c.SetLineCap(gg.LineCapRound)
-// 	c.SetRGB(0, 0, 0)
-
-// 	bs := make([]int, 3)
-// 	calcDigits(digit, bs)
-
-// 	var (
-// 		// xs = []float64{10, 50, 90}
-// 		// ys = []float64{20, 60, 100, 140, 180}
-
-// 		xs = []float64{15, 50, 85}
-// 		ys = []float64{30, 65, 100, 135, 170}
-// 	)
-
-// 	drawLineSegment(c, pt(xs[1], ys[0]), pt(xs[1], ys[2]))
-// 	drawLineSegment(c, pt(xs[1], ys[2]), pt(xs[1], ys[4]))
-
-// 	switch d := bs[0]; d {
-// 	case -1:
-// 		drawLineSegment(c, pt(xs[1], ys[0]), pt(xs[0], ys[1]))
-// 	case +1:
-// 		drawLineSegment(c, pt(xs[1], ys[0]), pt(xs[2], ys[1]))
-// 	default:
-// 	}
-
-// 	switch d := bs[1]; d {
-// 	case -1:
-// 		drawLineSegment(c, pt(xs[1], ys[2]), pt(xs[0], ys[1]))
-// 	case +1:
-// 		drawLineSegment(c, pt(xs[1], ys[2]), pt(xs[2], ys[1]))
-// 	default:
-// 	}
-
-// 	switch d := bs[2]; d {
-// 	case -1:
-// 		drawLineSegment(c, pt(xs[1], ys[2]), pt(xs[0], ys[3]))
-// 	case +1:
-// 		drawLineSegment(c, pt(xs[1], ys[2]), pt(xs[2], ys[3]))
-// 	default:
-// 	}
-
-// 	c.Stroke()
-// }
-
-// func pt(x, y float64) geom.Point2f {
-// 	return geom.MakePoint2f(x, y)
-// }
-
-// func drawLineSegment(c *gg.Context, a, b geom.Point2f) {
-// 	const k = 0.1
-// 	var (
-// 		a1 = geom.PtLerp(a, b, k)
-// 		b1 = geom.PtLerp(a, b, 1-k)
-// 	)
-// 	c.MoveTo(a1.X, a1.Y)
-// 	c.LineTo(b1.X, b1.Y)
-// }
--- geom/utils.go
+++ geom/utils.go
@@ -24,4 +24,8 @@ func lerp(v0, v1 float64, t float64) float64 {
 	return (1-t)*v0 + t*v1
 }
 
+func Lerp(v0, v1 float64, t float64) float64 {
+	return lerp(v0, v1, t)
+}
+
 //------------------------------------------------------------------------------
